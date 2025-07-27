f9e7994a0a1b6dc05e3058315303dcb3cdc627e3
devaqn <pedromiguelaqn@gmail.com>
2025-07-27 17:47:53 -0300

    primeiro commit

---
+++ Contexts/ExoContext.cs
@@ -0,0 +1,22 @@
+using Exo.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Exo.WebApi.Contexts
+{
+    public class ExoContext : DbContext
+    {
+        public DbSet<Projeto> Projetos { get; set; }  // <-- CORREÇÃO
+
+        public ExoContext() {}
+
+        public ExoContext(DbContextOptions<ExoContext> options) : base(options) {}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=ExoApi;Trusted_Connection=True;");
+            }
+        }
+    }
+}
